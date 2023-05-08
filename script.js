8a2b00f417fe335c185c26aabfbf8e46efc25590
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-08 13:29:02 +0300

    Object Orianted Programming (OOP) With JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 'use strict';
 
 //* Constructor Function and The New Operator
+// The Function() constructor creates Function objects. Calling the constructor directly can create functions dynamically
+
+// constructor creates functions which execute in the global scope only.
+
 const Person = function(firstName, birthYear) {
     //Instance properties
     this.firstName = firstName
     this.birthYear = birthYear
 
     // Never do this
-    this.calcAge = function() {
-        console.log(2037 - this.birthYear)
-    }
+    // this.calcAge = function() {
+    //     console.log(2037 - this.birthYear)
+    // }
 }
 
 const Tal = new Person('Tal', '1996')
@@ -18,10 +22,39 @@ console.log(Tal)
 // 1. New {} is created
 // 2. function is calld, this = {}
 // 3. linked to prototype
-// 4. ffunction automatically returns {}
+// 4. function automatically returns {}
 
 const matilda = new Person('Matilda', 2017)
 const Jack = new Person('Jack', 1975)
 console.log(matilda, Jack)
 
 console.log(Tal instanceof Person)
+
+
+//* Prototypes 
+// The prototype data property of a Function instance is used when the function is used as a constructor with the new operator. It will become the new object's prototype.
+
+// Operator - Reserved syntax consisting of punctuation or alphanumeric characters that carries out built-in functionality. For example, in JavaScript the addition operator ("+") adds numbers together and concatenates strings, whereas the "not" operator ("!") negates an expression — for example making a true statement return false.
+
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear)
+}
+
+Tal.calcAge()
+matilda.calcAge()
+
+console.log(Tal.__proto__) // (Deprecated)
+console.log(Tal.__proto__ === Person.prototype)
+
+console.log(Person.prototype.isPrototypeOf(Tal)) // The isPrototypeOf() method checks if an object exists in another object's prototype chain.
+console.log(Person.prototype.isPrototypeOf(matilda))
+console.log(Person.prototype.isPrototypeOf(Person))
+
+Person.prototype.species = 'Homo Sapiens'
+console.log(matilda.species, Tal.species)
+
+console.log(Tal.hasOwnProperty('firstName')) // The hasOwnProperty() method returns a boolean indicating whether the object has the specified property as its own property (as opposed to inheriting it).
+console.log(Tal.hasOwnProperty('birthYear'))
+console.log(Tal.hasOwnProperty('species'))
