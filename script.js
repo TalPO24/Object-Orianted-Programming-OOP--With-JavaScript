d6647e61e9a0ac5e968c2298b33416b8dccf7dca
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-10 14:14:29 +0300

    Object Orianted Programming (OOP) With JavaScript

--- script.js
+++ script.js
@@ -435,46 +435,83 @@ jay.introduce();
 jay.calcAge();
 */
 
-
+/*
 //* Another Class Exapmle
 class Account {
-    constructor(owner, currency, pin) {
-            this.owner = owner;
-            this.currency = currency;
-            this.pin = pin;
-            this.movements = [];
-            this.local = navigator.language
+    // 1) Public fields (instances)
+    locale = navigator.language;
 
-            console.log(`Thanks for opening an account, ${owner}`)
-        }
-        //* Public interface
+    // 2) Private fields (instances)
+    #movements = [];
+    #pin;
+
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.#pin = pin;
+
+        // Protected property
+        // this._movements = [];
+        // this.locale = navigator.language;
+
+        console.log(`Thanks for opening an account, ${owner}`);
+    }
+
+    // 3) Public methods
+
+    // Public interface
+    getMovements() {
+        return this.#movements;
+    }
+
     deposit(val) {
-        this.movements.push(val)
+        this.#movements.push(val);
+        return this;
     }
 
     withdraw(val) {
-        this.deposit(-val)
-    }
-    approvedLoan(val) {
-        return true
+        this.deposit(-val);
+        return this;
     }
 
     requestLoan(val) {
-        if (this.approvedLoan(val)) {
-            this.deposite(val)
-            console.log('Loan approved')
+        // if (this.#approveLoan(val)) {
+        if (this._approveLoan(val)) {
+            this.deposit(val);
+            console.log(`Loan approved`);
+            return this;
         }
+    }
+
+    static helper() {
+        console.log('Helper');
+    }
+
+    // 4) Private methods
+    // #approveLoan(val) {
+    _approveLoan(val) {
+        return true;
     }
 }
 
-const acc1 = new Account('Jonas', 'EUR', 1111)
+const acc1 = new Account('Jonas', 'EUR', 1111);
 
-// acc1.movements.push(250)
-// acc1.movements.push(-140)
-acc1.deposit(250)
-acc1.withdraw(140)
-acc1.requestLoan(1000)
-acc1.approvedLoan(1000)
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+// acc1.approveLoan(1000);
 
-console.log(acc1)
-console.log(acc1.pin)
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
+//* Encapsulatio: Protected Properties and Methods 
+
+//* Encapsulation is the packing of data and functions into one component (for example, a class) and then controlling access to that component to make a "blackbox" out of the object. Because of this, a user of that class only needs to know its interface (that is, the data and functions exposed outside the class), not the hidden implementation.
+*/
