f2f530fae2ccfda916a7d2fb9ac77452973cf065
Tal Poris <119438047+TalPO24@users.noreply.github.com>
2023-05-10 13:59:12 +0300

    Object Orianted Programming (OOP) With JavaScript

--- script.js
+++ script.js
@@ -404,6 +404,7 @@ martha.introduce()
 martha.clacAge()
 */
 
+/*
 //* Inheritance Between "Classes": Object.create
 const PersonProto = {
     calcAge() {
@@ -432,3 +433,48 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+
+
+//* Another Class Exapmle
+class Account {
+    constructor(owner, currency, pin) {
+            this.owner = owner;
+            this.currency = currency;
+            this.pin = pin;
+            this.movements = [];
+            this.local = navigator.language
+
+            console.log(`Thanks for opening an account, ${owner}`)
+        }
+        //* Public interface
+    deposit(val) {
+        this.movements.push(val)
+    }
+
+    withdraw(val) {
+        this.deposit(-val)
+    }
+    approvedLoan(val) {
+        return true
+    }
+
+    requestLoan(val) {
+        if (this.approvedLoan(val)) {
+            this.deposite(val)
+            console.log('Loan approved')
+        }
+    }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111)
+
+// acc1.movements.push(250)
+// acc1.movements.push(-140)
+acc1.deposit(250)
+acc1.withdraw(140)
+acc1.requestLoan(1000)
+acc1.approvedLoan(1000)
+
+console.log(acc1)
+console.log(acc1.pin)
